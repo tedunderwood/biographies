21c4e201dccbe88ad74b0b612a981882db510e6d
Ryan Dubnicek <rdubnic2@illinois.edu>
2017-07-11 11:23:58 -0500

    Add files via upload
    Second round of fixes.

--- code/booknlp-file-check.py
+++ code/booknlp-file-check.py
@@ -18,8 +18,8 @@ for item in hold_list:
     hold_item_name = item[:-4]
     clean_hold_list.append(hold_item_name)
 
-for item in clean_copied_list:
-    if item in clean_hold_list:
+for item in clean_hold_list:
+    if item in clean_copied_list:
         continue
     else:
         miss_list.append(item)
