b1f832e0557ea49cf0b590975fc32a56afedeb64
Ryan Dubnicek <rdubnic2@illinois.edu>
2017-07-11 15:49:00 -0500

    Add files via upload
    Fourth round of fixes.

--- code/booknlp-file-check.py
+++ code/booknlp-file-check.py
@@ -1,27 +1,29 @@
 import os
 
-copied_list = os.listdir('/media/secure_volume/output/')
+cop_list = os.listdir('/media/secure_volume/output/')
 
-hold_list = os.listdir('/media/secure_volume/holding_folder/')
+hold_list = os.listdir('/media/secure_volume/2holding_folder/')
 
 miss_list = []
 
-clean_copied_list = []
+clean_cop_list = []
 
 clean_hold_list = []
 
-for item1 in copied_list:
-    item_name = item1[:-5]
-    clean_copied_list.append(item_name)
+for cop_item in cop_list:
+    # file suffix is .book for these files
+    cop_item_name = str(cop_item[:-5]) # removing extension
+    clean_cop_list.append(cop_item_name)
 
-for item2 in hold_list:
-    hold_item_name = item2[:-4]
+for hold_item in hold_list:
+    # file suffix is .zip for these fiels
+    hold_item_name = str(hold_item[:-4]) # removing file extension
     clean_hold_list.append(hold_item_name)
 
-for item3 in clean_hold_list:
-    if item3 in clean_copied_list:
+for check_item in clean_hold_list:
+    if check_item in clean_copied_list:
         continue
     else:
-        miss_list.append(item3)
+        miss_list.append(check_item)
 
 print(str(len(miss_list)),'files not extracted.')
