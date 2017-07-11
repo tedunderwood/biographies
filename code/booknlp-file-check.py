c9af78db3251ff02741f257158c0e98bcd74a1a7
Ryan Dubnicek <rdubnic2@illinois.edu>
2017-07-11 11:07:29 -0500

    Add files via upload
    Modified to drop file extensions, then check.

--- code/booknlp-file-check.py
+++ code/booknlp-file-check.py
@@ -6,11 +6,22 @@ hold_list = os.listdir('/media/secure_volume/holding_folder/')
 
 miss_list = []
 
+clean_copied_list = []
+
+clean_hold_list = []
+
 for item in copied_list:
-    # print(item)
-    if item in hold_list:
+    item_name = item[:-5]
+    clean_copied_list.append(item_name)
+
+for item in hold_list:
+    hold_item_name = item[:-4]
+    clean_hold_list.append(hold_item_name)
+
+for item in clean_copied_list:
+    if item in clean_hold_list:
         continue
     else:
         miss_list.append(item)
 
-print(len(miss_list),'files not extracted.')
+print(str(len(miss_list)),'files not extracted.')
